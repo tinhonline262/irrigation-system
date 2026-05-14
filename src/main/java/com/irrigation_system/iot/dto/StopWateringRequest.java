c3ae0a60b39347c9fdc37e22a1d65e0d36a0dbf5
NguyenAn3001 <nguyenlinh357361@gmail.com>
2026-05-14 11:19:23 +0700

    fix stop watering

--- src/main/java/com/irrigation_system/iot/dto/StopWateringRequest.java
+++ src/main/java/com/irrigation_system/iot/dto/StopWateringRequest.java
@@ -1,6 +1,5 @@
 package com.irrigation_system.iot.dto;
 
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
@@ -9,7 +8,6 @@ import lombok.AccessLevel;
 @Data
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class StopWateringRequest {
-    @NotNull(message = "Water amount is required")
     @Positive(message = "Water amount must be greater than 0")
     Float waterAmountMl;
 }
